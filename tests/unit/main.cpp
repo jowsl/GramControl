09ac7db16e2379e9819f3c455a21eeeda9544b2d
DavidSouza99 <daviddasou@gmail.com>
2026-07-07 12:53:02 -0300

    Corrige main.cpp dos testes: adiciona QCoreApplication

--- tests/unit/main.cpp
+++ tests/unit/main.cpp
@@ -1,15 +1,19 @@
+#include <QCoreApplication>
 #include "test_login_reg.h"
 #include "test_persistencia.h"
 #include "test_PrecoBase.h"
 #include "test_Cliente.hpp"
 #include "test_persistencia_cliente.hpp"
 
-int main() {
+int main(int argc, char *argv[]) {
+    QCoreApplication app(argc, argv);
+
     rodarTestesUnitarios();
     teste_persistencia_update_afeta_linha();
     teste_atualizacao_preco_valido();
     teste_cliente_valido();
     teste_cliente_sem_nome_ou_telefone();
     teste_persistencia_insere_cliente();
+
     return 0;
 }
